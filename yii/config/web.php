9a6aca4591ab6702877d71bebe477ee819f6d6d1
kagios <git@kagios.gr>
2021-06-22 02:47:12 +0300

    oo

--- yii/config/web.php
+++ yii/config/web.php
@@ -140,7 +140,7 @@ $config = [
     ],
     'params' => $params,
 ];
-
+/* 
 if (YII_ENV_DEV) {
     // configuration adjustments for 'dev' environment
     $config['bootstrap'][] = 'debug';
@@ -156,6 +156,6 @@ if (YII_ENV_DEV) {
         // uncomment the following to add your IP if you are not connecting from localhost.
         //'allowedIPs' => ['127.0.0.1', '::1'],
     ];
-}
+} */
 
 return $config;
