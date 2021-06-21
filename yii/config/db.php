65e3e6ecd7388485d5c198b38dc313dbcfac9748
kagios <git@kagios.gr>
2021-06-21 16:36:46 +0300

    db

--- yii/config/db.php
+++ yii/config/db.php
@@ -3,9 +3,9 @@
 return [
      
      'class' => 'yii\db\Connection',
-     'dsn' => YII_ENV == 'dev' ? 'mysql:host=localhost;dbname=philocalist_db' : 'mysql:host=localhost;dbname=philocalist_db',
-     'username' => YII_ENV == 'dev' ? 'root' : 'kagiosg_myqsql_user',
-     'password' => YII_ENV == 'dev' ? 'root' : '?p#P&(vXSkPe',
+     'dsn' => YII_ENV == 'dev' ? 'mysql:host=localhost;dbname=philocalist_db' : 'mysql:host=localhost;dbname=n137576arv_philocalist_db',
+     'username' => YII_ENV == 'dev' ? 'root' : 'n137576arv_philocalist_db_user',
+     'password' => YII_ENV == 'dev' ? 'root' : '?l2mG0o9',
      'charset' => 'utf8',
  
      // Schema cache options (for production environment)
