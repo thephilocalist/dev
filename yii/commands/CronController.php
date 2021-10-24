7665b955419db69a2d6697d8576bf495dd495a02
kagios <git@kagios.gr>
2021-10-24 15:58:10 +0300

    cron

--- yii/commands/CronController.php
+++ yii/commands/CronController.php
@@ -4,26 +4,26 @@
  * @copyright Copyright (c) 2008 Yii Software LLC
  * @license http://www.yiiframework.com/license/
  */
+/* /opt/plesk/php/7.4/bin/php /var/www/vhosts/thephilocalist.gr/dev.thephilocalist.gr/yii  cron/publish  */
 
 namespace app\commands;
 
 use yii\console\Controller;
-use Yii;
 use app\models\db\Articles;
 
 
 class CronController extends Controller {
 
-    function actionPublish()
+    public function actionPublish()
     {
         $date = time();
-        $model = Articles::find()->andWhere(['published' => 0])->all();
+        $model = Articles::find()->andWhere(['published' => 0])->andWhere(['<', 'publish_at', $date])->all();
 
         foreach($model as $article) {            
             $article->published = '1';
             $article->save();
             
-            // */5 * * * * kathe 5 lepta php cron/publish  ->andWhere(['<', 'publish_at', $date])
+            // */5 * * * * kathe 5 lepta php cron/publish 
         }
     }
 
